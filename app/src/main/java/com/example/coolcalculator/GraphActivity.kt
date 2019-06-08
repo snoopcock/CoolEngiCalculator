3fd33f4f6045f5f09136086f22b000c7c02217f8
klimoza <48102299+klimoza@users.noreply.github.com>
2019-06-08 12:11:54 +0300

    Merge branch 'master' of https://github.com/snoopcock/CoolEngiCalculator into klimoza
    # Conflicts:
    #	app/src/main/res/drawable/pi.png
    #	app/src/main/res/layout-land/activity_engi_calc.xml

--- app/src/main/java/com/example/coolcalculator/GraphActivity.kt
+++ app/src/main/java/com/example/coolcalculator/GraphActivity.kt
@@ -3,9 +3,6 @@ package com.example.coolcalculator
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
 import kotlinx.android.synthetic.main.activity_graph.*
 
 class   GraphActivity : AppCompatActivity() {
@@ -18,15 +15,7 @@ class   GraphActivity : AppCompatActivity() {
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        var data : List<Pair<Int, Int>> = List(10) { i -> Pair(i, i * i)}
-        var entries : List<Entry> = emptyList()
-        for(i in data)
-            entries = entries.plus(Entry(i.first.toFloat(), i.second.toFloat()))
-        var dataSet : LineDataSet = LineDataSet(entries, "TestObj")
 
-        var lineData : LineData = LineData(dataSet)
-        chart.setData(lineData)
-        chart.invalidate()
     }
 
 }
