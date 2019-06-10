2c8dfb42575869dc35bd10f37aa36a9e6cc92c86
frodeey <daniil.dubrovin10@mail.ru>
2019-06-10 14:56:27 +0300

    graph enter buttons funs

--- app/src/main/java/com/example/coolcalculator/EnterGraphActivity.kt
+++ app/src/main/java/com/example/coolcalculator/EnterGraphActivity.kt
@@ -2,6 +2,9 @@ package com.example.coolcalculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_engi_calc.*
+import kotlinx.android.synthetic.main.activity_enter_graph.*
+
 
 class EnterGraphActivity : AppCompatActivity() {
 
@@ -11,5 +14,40 @@ class EnterGraphActivity : AppCompatActivity() {
 
         getSupportActionBar()!!.hide()
 
+        tvOne1.setOnClickListener {appendOnFunction("1",true)}
+        tvTwo1.setOnClickListener {appendOnFunction("2",true)}
+        tvThree1.setOnClickListener {appendOnFunction("3",true)}
+        tvFour1.setOnClickListener {appendOnFunction("4",true)}
+        tvFive1.setOnClickListener {appendOnFunction("5",true)}
+        tvSix1.setOnClickListener {appendOnFunction("6",true)}
+        tvSeven1.setOnClickListener {appendOnFunction("7",true)}
+        tvEight1.setOnClickListener {appendOnFunction("8",true)}
+        tvNine1.setOnClickListener {appendOnFunction("9",true)}
+        tvDot1.setOnClickListener {appendOnFunction(".",true)}
+        tvZero1.setOnClickListener {appendOnFunction("0",true)}
+
+        tvPlus1.setOnClickListener {appendOnFunction("+", false)}
+        tvMinus1.setOnClickListener {appendOnFunction("-", false)}
+        tvMul1.setOnClickListener {appendOnFunction("*", false)}
+        tvDevide1.setOnClickListener {appendOnFunction("/", false)}
+        tvOpen1.setOnClickListener {appendOnFunction("(", false)}
+        tvClose1.setOnClickListener {appendOnFunction(")", false)}
+        tvX.setOnClickListener {appendOnFunction("x", false)}
+
+        tvClear1.setOnClickListener{
+            tvFunction.text = ""
+        }
+
+        tvBack1.setOnClickListener{
+            val string = tvFunction.text.toString()
+            if(string.isNotEmpty()){
+                tvFunction.text = string.substring(0, string.length-1)
+            }
+        }
+
+    }
+
+    fun appendOnFunction(string : String, canClear : Boolean) {
+        tvFunction.append(string)
     }
 }
