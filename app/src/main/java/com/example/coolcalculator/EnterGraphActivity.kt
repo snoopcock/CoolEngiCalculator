85442a0a2b68ebee1abb98479af9038fa0990078
frodeey <daniil.dubrovin10@mail.ru>
2019-06-11 15:08:22 +0300

    gvn

--- app/src/main/java/com/example/coolcalculator/EnterGraphActivity.kt
+++ app/src/main/java/com/example/coolcalculator/EnterGraphActivity.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_engi_calc.*
 import kotlinx.android.synthetic.main.activity_enter_graph.*
+import android.R.id.edit
+import android.content.Intent
+import android.content.SharedPreferences.Editor
+import android.preference.PreferenceManager
+
+
 
 
 class EnterGraphActivity : AppCompatActivity() {
@@ -33,21 +39,35 @@ class EnterGraphActivity : AppCompatActivity() {
         tvOpen1.setOnClickListener {appendOnFunction("(", false)}
         tvClose1.setOnClickListener {appendOnFunction(")", false)}
         tvX.setOnClickListener {appendOnFunction("x", false)}
+        tvDegree.setOnClickListener {appendOnFunction("^",  false)}
 
-        tvClear1.setOnClickListener{
-            tvFunction.text = ""
-        }
+
+        val s_pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val edit = s_pref.edit()
+        tvEnter.setOnClickListener{
+            edit.putString("textview_text","some text")
+            edit.commit()
+
+            val intent = Intent(this, GraphActivity::class.java)
+            var ch = intent.getIntExtra("che", -1)
+            ch++
+            intent.putExtra("TAGgraph", tvFunction.text.toString())
+            intent.putExtra("che", ch)
+
+            startActivity(intent)
+            }
 
         tvBack1.setOnClickListener{
             val string = tvFunction.text.toString()
             if(string.isNotEmpty()){
-                tvFunction.text = string.substring(0, string.length-1)
+            tvFunction.text = string.substring(0, string.length-1)
             }
         }
 
     }
 
+
     fun appendOnFunction(string : String, canClear : Boolean) {
         tvFunction.append(string)
-    }
+        }
 }
--- app/src/main/java/com/example/coolcalculator/GraphActivity.kt
+++ app/src/main/java/com/example/coolcalculator/GraphActivity.kt
@@ -2,6 +2,7 @@ package com.example.coolcalculator
 
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_graph.*
@@ -16,10 +17,41 @@ class GraphActivity : AppCompatActivity() {
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        var ch : Int = 0
+
         buttonPlus.setOnClickListener {
             val intent = Intent(this, EnterGraphActivity::class.java)
+            intent.putExtra("che", ch)
             startActivity(intent)
 
         }
+
+        if ((intent.hasExtra("TAGgraph"))&&(intent.getIntExtra("che", -1)==1)) {
+            var data = intent.getStringExtra("TAGgraph")
+            tvY1.text = "1) y = $data"
+            tvY1.setTextColor(Color.WHITE)
+            tvShow1.setBackgroundResource(R.color.actionButton)
+            tvDelete1.setBackgroundResource(R.color.actionButton)
+
+        }
+
+        if ((intent.hasExtra("TAGgraph"))&&(intent.getIntExtra("che", -1)==2)) {
+            var data = intent.getStringExtra("TAGgraph")
+            tvY2.text = "2) y = $data"
+            tvY2.setTextColor(Color.WHITE)
+            tvShow2.setBackgroundResource(R.color.actionButton)
+            tvDelete2.setBackgroundResource(R.color.actionButton)
+        }
+
+        if ((intent.hasExtra("TAGgraph"))&&(intent.getIntExtra("che", -1)==3)) {
+            var data = intent.getStringExtra("TAGgraph")
+            tvY3.text = "3) y = $data"
+            tvY3.setTextColor(Color.WHITE)
+            tvShow3.setBackgroundResource(R.color.actionButton)
+            tvDelete3.setBackgroundResource(R.color.actionButton)
+        }
+
+
+
     }
 }
